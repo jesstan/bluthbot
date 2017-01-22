724031470462b4489930ff10212ccc9f506cbca3
jesstan <jessicalouisetan@gmail.com>
2017-01-21 22:39:32 -0800

    Added log for testing

--- bluthbot.js
+++ bluthbot.js
@@ -25,7 +25,7 @@ This is a sample Slack Button application that adds a bot to one or many slack t
 var Botkit = require('botkit');
 var port = process.env.PORT || process.env.port;
 var http = require("http");
-
+console.log("INSIDE BLUTHBOT.JS");
 setInterval(function() {
     http.get("https://bluthbot.herokuapp.com/");
 }, 1200000); // pings heroku every 20 minutes (1200000) to keep it awake
