513804e80bc1eb372f0feebf9812c0b89cae1ebc
jesstan <jessicalouisetan@gmail.com>
2017-02-12 20:11:57 -0800

    Trying out regex hello function

--- bluthbot.js
+++ bluthbot.js
@@ -127,15 +127,15 @@ controller.on('rtm_close',function(bot) {
 });
 
 //TODO: look into modifying botkit.hears method to take regex instead of strings
-// controller.hears(['^((.*\s)|(\s?))hello((\s.*)|(\s?))$','^((.*\s)|(\s?))hi((\s.*)|(\s?))$','^((.*\s)|(\s?))annyong((\s.*)|(\s?))$'],
-//   ['direct_message','direct_mention'],function(bot,message) {
-//     console.log("heard: ", message);
-//   bot.reply(message,'Annyong!');
-// });
-
-controller.hears(['hello','hi','annyong'],['direct_message','direct_mention'],function(bot,message) {
+controller.hears(['^((.*\s)|(\s?))hello((\s.*)|(\s?))$','^((.*\s)|(\s?))hi((\s.*)|(\s?))$','^((.*\s)|(\s?))annyong((\s.*)|(\s?))$'],
+  ['direct_message','direct_mention'],function(bot,message) {
+    console.log("heard: ", message);
   bot.reply(message,'Annyong!');
 });
+
+/*controller.hears(['hello','hi','annyong'],['direct_message','direct_mention'],function(bot,message) {
+  bot.reply(message,'Annyong!');
+});*/
 
 controller.hears(['ham'],['direct_message','direct_mention','ambient'],function(bot,message) {
   bot.reply(message, "So watery. And yet there's a SMACK of ham to it.");
