049671a42152831497321fd33d1a432e67ef9b93
jesstan <jessicalouisetan@gmail.com>
2017-02-12 20:32:24 -0800

    Trying out regex stuff

--- bluthbot.js
+++ bluthbot.js
@@ -123,7 +123,7 @@ controller.on('rtm_close',function(bot) {
   // you may want to attempt to re-open
 });
 
-controller.hears(['^((.*\\s)|(\\s?))hello((\\s.*)|(\\s?)|(\\.)|(\\!)|(\\?)|(\\,))$','^((.*\\s)|(\\s?))hi((\\s.*)|(\\s?))$','^((.*\\s)|(\\s?))annyong((\\s.*)|(\\s?))$'],
+controller.hears(['^((.*\\s)|(\\s?))hello((((\\.)|(\\!)|(\\?)|(\\,))*(\\s.*)?)|(\\s?))$','^((.*\\s)|(\\s?))hi((\\s.*)|(\\s?))$','^((.*\\s)|(\\s?))annyong((\\s.*)|(\\s?))$'],
   ['direct_message','direct_mention'],function(bot,message) {
     console.log("heard: ", message);
   bot.reply(message,'Annyong!');
